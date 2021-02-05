ed94ff3b1af1e24dc7811bd8f3fc38cebcc0beb1
Yyani <yamouni.yani@gmail.com>
2021-02-05 10:31:15 +0100

    Updating the java-class of the backend module

--- backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/BackendService.java
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/BackendService.java
@@ -1,4 +1,34 @@
 package edu.episen.si.ing1.pds.backend.server;
 
+
+import org.apache.commons.cli.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BackendService {
+    private final static Logger logger = LoggerFactory.getLogger(BackendService.class.getName());
+
+    public static void main(String[] args) throws Exception {
+        final Options options = new Options();
+        final Option testMode = Option.builder().longOpt("testMode").build();
+        options.addOption(testMode);
+
+        final Option maxConnection = Option.builder().longOpt("maxConnection").hasArg().build();
+        options.addOption(maxConnection);
+
+        final CommandLineParser parser = new DefaultParser();
+        final CommandLine commandLine = parser.parse(options, args);
+
+        boolean itestMode = false;
+        if (commandLine.hasOption("testMode"))
+            itestMode = true;
+
+        int iMaxConnection = 10;
+        if (commandLine.hasOption("maxConnection"))
+            iMaxConnection = Integer.parseInt(commandLine.getOptionValue("maxConnection"));
+
+        logger.info("Backend Service is running (testMode = " + itestMode + ") , (maxconnection = " + iMaxConnection + "}.");
+
+    }
 }
